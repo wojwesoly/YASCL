3eb63650ea8b44886c5acd35be4046d39aac1026
wojwesoly <wojwesoly@gmail.com>
2022-04-18 00:54:22 +0200

    Updated convert script.

---
+++ md/convert.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DIR="/home/wojwesoly/Documents/Notes/YASCL"
+
+for FILE in $DIR/md/*.md; do
+    NAME=$(echo -n $FILE | head -c-3)
+    PDF_NAME=$(basename $NAME) 
+    
+    echo "Creating $NAME.html..."
+    pandoc $FILE -o $NAME.html
+
+    echo "Creating $DIR/pdf/$PDF_NAME.pdf..."
+    prince $NAME.html -o $DIR/pdf/$PDF_NAME.pdf
+
+    #echo "NAME: $NAME"
+    #echo "PDF_NAME: $PDF_NAME"
+
+done
+
+# .html files are only used for conversion with prince.
+echo -e "\nDeleting all .html files..."
+/usr/bin/rm $DIR/md/*.html
---
+++ push.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+./md/convert.sh
+
+git add .
+git commit -m $1
+git push
